abb10750721b03b87905d4b7df569ca20820b5d4
miki5ki <53338596+miki5ki@users.noreply.github.com>
2022-02-28 21:38:58 +0900

    ✨バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  validates :name, presence: true,length: { maximum: 999 }
+  validates :email, presence: true,length: { maximum: 256 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
