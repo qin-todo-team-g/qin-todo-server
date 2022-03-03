77e58302cb3d478b61901739273e5de4410bb796
igarashi kazuki <sousiki.links@gmail.com>
2022-03-04 02:15:04 +0900

    update: spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email                 {"test@gmail.com"}
-    password              {"111111"}
-    password_confirmation {"111111"}
+    name { 'user' }
+    sequence(:email) { |n| "test#{n}@email.com" }
+    password { 'password' }
   end
 end
