844ec5759376f1364998282420ef636c4bfe17a8
miki5ki <53338596+miki5ki@users.noreply.github.com>
2022-03-10 09:03:52 +0900

    ✨ルーティングの更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
-  }
+  namespace :api do
+  namespace :v1 do
+ mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+     registrations: 'api/v1/auth/registrations'
+ }
+ resources :users, only: [:index, :show, :create, :update, :destroy]
+ end
+ end
 end
