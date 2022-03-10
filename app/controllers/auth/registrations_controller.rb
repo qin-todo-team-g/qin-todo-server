4687188a2ee08005167856d3f767ed2ea2096592
miki5ki <53338596+miki5ki@users.noreply.github.com>
2022-03-10 09:03:16 +0900

    ♻️deviseのcontrollerの修正

--- app/controllers/auth/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  private
-
-  def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation)
-  end
-end
