0731b543aab083df42caff608949b8a9f249df12
igarashi kazuki <sousiki.links@gmail.com>
2022-03-21 00:32:50 +0900

    update: doneアクションの削除、showアクションの追加

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :get_user, only: %i[index create]
-  before_action :get_task, only: %i[update done destroy]
+  before_action :get_task, only: %i[show update destroy]
 
   def index
     tasks = @user.tasks.all
@@ -27,19 +27,18 @@ class Api::V1::TasksController < ApplicationController
     end
   end
 
-  def update
-    if @task.update(task_params)
+  def show
+    if @task.present?
       render json: {
         task: @task
       }, status: :ok
     else
-      render json: {}, status: :internal_server_error
+      render json: {}, status: :no_content
     end
   end
 
-  def done
-    @task.toggle(:is_done)
-    if @task.save
+  def update
+    if @task.update(task_params)
       render json: {
         task: @task
       }, status: :ok
@@ -55,7 +54,7 @@ class Api::V1::TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:task_type, :title)
+    params.require(:task).permit(:task_type, :title, :is_done)
   end
 
   def get_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks
-      patch '/tasks/:id/done', to: 'tasks#done' 
     end
   end
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
