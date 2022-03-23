b9488c2b37685352f35fad4405ad15d04f841c19
igarashi kazuki <sousiki.links@gmail.com>
2022-03-23 21:00:38 +0900

    delete: devise及びdevise_token_auth関連のリソースを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_133343) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "system_settings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.boolean "is_dark", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_system_settings_on_user_id"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.integer "task_type", null: false
-    t.boolean "is_done", default: false, null: false
-    t.string "title", default: "", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "name", default: "", null: false
-    t.string "email", default: "", null: false
-    t.json "tokens"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-  end
-
-  add_foreign_key "system_settings", "users"
-  add_foreign_key "tasks", "users"
 end
